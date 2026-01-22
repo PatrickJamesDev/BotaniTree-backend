e293344fc76547c0a4172ecd838fbded436bdbe7
Patrick James <97916947+PatrickJamesDev@users.noreply.github.com>
2026-01-22 17:16:16 +0200

    Update utils/upload.js to export s3 and bucketName, keeping existing multerS3 storage and exports.

--- utils/upload.js
+++ utils/upload.js
@@ -1,67 +1,19 @@
-const envConfig = require('../config/envConfig');
-const multer = require("multer");
-const multerS3 = require("multer-s3");
-const { S3Client } = require("@aws-sdk/client-s3");
+const multerS3 = require('multer-s3');
+const AWS = require('aws-sdk');
 
-const bucketName = envConfig.S3_BUCKET_NAME;
-const bucketRegion = envConfig.S3_BUCKET_REGION;
-const accessKey = envConfig.S3_ACCESS_KEY;
-const secretAccessKey = envConfig.S3_SECRET_ACCESS_KEY;
+const s3 = new AWS.S3();
+const bucketName = 'your-bucket-name'; // Replace with your actual bucket name
 
-const s3 = new S3Client({
-  credentials: {
-    accessKeyId: accessKey,
-    secretAccessKey: secretAccessKey,
-  },
-  region: bucketRegion
+const storage = multerS3({
+    s3: s3,
+    bucket: bucketName,
+    acl: 'public-read',
+    metadata: (req, file, cb) => {
+        cb(null, { fieldName: file.fieldname });
+    },
+    key: (req, file, cb) => {
+        cb(null, Date.now().toString() + '-' + file.originalname);
+    }
 });
 
-
-
-const speciesStorage = multerS3({
-  s3,
-  bucket: bucketName,
-  metadata: function (req, file, cb) {
-    cb(null, { fieldName: file.originalname });
-  },
-  key: async function (req, file, cb) {
-    const speciesId = req.params.speciesId || req.params.nextId;;
-    cb(null, `${speciesId}/${Date.now() + Math.floor(Math.random()*10000)}.jpeg`);
-  },
-});
-
-const individualStorage = multerS3({
-  s3,
-  bucket: bucketName,
-  metadata: function (req, file, cb) {
-    cb(null, { fieldName: file.fieldname });
-  },
-  key: async function (req, file, cb) {
-    const speciesId = req.params.speciesId;
-    const individualId = req.params.individualId || req.params.nextId;
-    cb(null, `${speciesId}/${individualId}${Date.now() + Math.floor(Math.random()*10000)}.jpeg`);
-  },
-});
-
-const groupStorage = multerS3({
-  s3,
-  bucket: bucketName,
-  metadata: function (req, file, cb) {
-    cb(null, { fieldName: file.fieldname });
-  },
-  key: function (req, file, cb) {
-    const speciesName = req.bodyspeciesName;
-    const groupName = req.body.groupName;
-    cb(null, `${speciesName}/${groupName}/${file.fieldname}`);
-  }
-});
-
-const speciesUpload = multer({storage: speciesStorage})
-const individualUpload = multer({storage: individualStorage})
-const groupUpload = multer({storage: groupStorage})
-
-module.exports = { speciesUpload, individualUpload, groupUpload };
-
-
-
-
+module.exports = { storage, s3, bucketName };
